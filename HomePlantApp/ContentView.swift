a952572e958c77dabd3d461ece984433ff5a26b4
mehmeterenortak <mehmet_310gs@hotmail.com>
2024-07-08 19:31:15 +0300

    Initial Commit

---
+++ HomePlantApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomePlantApp
+//
+//  Created by Mehmet eren Ortak on 8.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomePlantApp/HomePlantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePlantAppApp.swift
+//  HomePlantApp
+//
+//  Created by Mehmet eren Ortak on 8.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HomePlantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
